4d7ae6ebb71dc6a077bcd3e63d1c480b040faa79
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-11-12 20:49:54 +0100

    Remove default component

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Matcha</h1>
+<p>This is the humble beginning of our Matcha web app</p>
