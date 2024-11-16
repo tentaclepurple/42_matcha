c077b0b74080e36462aa0fbc11a15d314329e85f
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-11-16 23:04:49 +0100

    Add signup page

---
+++ front/src/lib/components/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	type ButtonType = 'button' | 'submit' | 'reset';
+	type ButtonLevel = 'primary' | 'secondary';
+
+	let {
+		children,
+		level = 'primary' as ButtonLevel,
+		type = 'button' as ButtonType,
+		...restProps
+	} = $props();
+
+	let styles =
+		level === 'primary'
+			? 'rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700'
+			: 'bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded';
+</script>
+
+<button {type} {...restProps} class={styles}>
+	{@render children()}
+</button>
---
+++ front/src/routes/signup/+page.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+	import Form from '$lib/components/Form.svelte';
+
+	import { goto } from '$app/navigation';
+
+	let error: string = '';
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		console.log('submitting form');
+
+		const form = e.target;
+		const formData = new FormData(form);
+
+		const response = await fetch('http://localhost:5000/api/users/register', {
+			method: 'POST',
+			body: JSON.stringify(Object.fromEntries(formData)),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		if (!response.ok) {
+			switch (response.status) {
+				case 409:
+					error = 'User already exists';
+					break;
+			}
+		}
+	};
+
+	const handleCancel = () => {
+		goto('/');
+	};
+</script>
+
+<h1>Create a new account</h1>
+<div>
+	<Form onSubmit={handleSubmit}>
+		<label>
+			Username
+			<input type="text" id="username" name="username" value="testuser" />
+		</label>
+
+		<label>
+			First name
+			<input type="text" id="first_name" name="first_name" value="Test" />
+		</label>
+		<label>
+			Last name
+			<input type="text" id="last_name" name="last_name" value="User" />
+		</label>
+
+		<label>
+			Email
+			<input type="email" id="email" name="email" value="ibanmontero@gmail.com" />
+		</label>
+		<label>
+			Password
+			<input type="password" id="password" name="password" value="test123" />
+		</label>
+
+		<div>
+			<Button type="button" level="secondary" onclick={handleCancel}>Cancel</Button>
+			<Button type="submit" level="primary">Sign up</Button>
+		</div>
+	</Form>
+	{#if error}
+		<p class="text-red-500 mt-4">{error}</p>
+	{/if}
+</div>
