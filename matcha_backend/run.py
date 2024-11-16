af5a853cc16eec560ad7c601071ad4227508ccdf
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-11-16 22:52:01 +0100

    Enable CORS on BE

--- matcha_backend/run.py
+++ matcha_backend/run.py
@@ -1,7 +1,8 @@
 from app import create_app
+from flask_cors import CORS
 
 app = create_app()
-
+CORS(app)  # TODO investigate if this is a security risk
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
