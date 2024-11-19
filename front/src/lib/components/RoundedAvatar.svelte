90786097ab186c4b0351a666d98c84fe156eb6a8
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-11-19 21:33:12 +0100

    Create RoundedAvatar component

---
+++ front/src/lib/components/RoundedAvatar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	const { alt = '', src, ...restProps } = $props();
+</script>
+
+<div class="rounded-full border-2 border-teal-500 p-4">
+	<img {src} {alt} {...restProps} />
+</div>
--- front/src/routes/profile/+page.svelte
+++ front/src/routes/profile/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
 	import PageWrapper from '$lib/components/PageWrapper.svelte';
+	import RoundedAvatar from '$lib/components/RoundedAvatar.svelte';
 	import getServerAsset from '$lib/utils/get-server-asset';
 
 	interface ProfileData {
@@ -20,9 +21,7 @@
 
 	<div>
 		<div class="flex items-center gap-4">
-			<div class="rounded-full border-2 border-teal-500 p-4">
-				<img src={getServerAsset(data.profilePictureUrl)} alt="" class="w-16" />
-			</div>
+			<RoundedAvatar src={getServerAsset(data.profilePictureUrl)} alt="" class="w-16" />
 			<div>
 				<h2>{data.firstName} {data.lastName}</h2>
 				<p>{data.username}</p>
