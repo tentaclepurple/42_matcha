6d29df3ea7e60aaab6a19ac153d6e29a9457a9a1
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-11-22 19:10:29 +0100

    added custom cors config

--- matcha_backend/run.py
+++ matcha_backend/run.py
@@ -2,7 +2,25 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)  # TODO investigate if this is a security risk
+
+cors_config = {
+    "origins": [
+        "http://localhost:5173",  # development frontend
+        "http://127.0.0.1:5173"   # common alias
+    ],
+    "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # methods allowed
+    "allow_headers": [
+        "Content-Type",
+        "Authorization",
+        "Access-Control-Allow-Credentials"
+    ],
+    "supports_credentials": True,  # mandatory for cookies to be allowed
+    "max_age": 3600  # time in seconds that preflight response should be cached
+}
+
+CORS(app, resources={
+    r"/api/*": cors_config # applies to all API routes
+})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
