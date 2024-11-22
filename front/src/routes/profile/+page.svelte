571acb941d5bf913060270e92b5903fdbd7fd8e6
Nicolas Gasco <nicolas.gasco@qonto.com>
2024-11-22 14:27:03 +0100

    Add logic to upload profile pictures

--- front/src/routes/profile/+page.svelte
+++ front/src/routes/profile/+page.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
 	import PageWrapper from '$lib/components/PageWrapper.svelte';
-	import type UserProfileData from '$lib/interfaces/user-profile-data.interface';
+	import { userProfileData } from '$lib/stores/user-profile-data';
 	import PhotoGallery from './PhotoGallery.svelte';
 
-	let {
-		data
-	}: {
-		data: {
-			profileData: UserProfileData;
-		};
-	} = $props();
+	const currentProfileData = $userProfileData;
 
-	const avatarUrl = data.profileData.photos.filter((photo) => photo.is_profile)[0]?.url;
-	const photos = data.profileData.photos.filter((photo) => !photo.is_profile);
+	const avatarUrl = $derived(
+		$userProfileData?.photos.filter((photo) => photo.is_profile)[0]?.url ?? ''
+	);
+	const photos = $derived($userProfileData?.photos.filter((photo) => !photo.is_profile) ?? []);
 </script>
 
 <PageWrapper>
@@ -24,8 +20,8 @@
 	<div class="mb-4 flex w-fit flex-col items-start">
 		<PhotoGallery {avatarUrl} {photos} />
 		<div class="flex w-56 items-baseline justify-between">
-			<p class="text-3xl">{data.profileData.username}</p>
-			<p class="text-2xl">{data.profileData.age}</p>
+			<p class="text-3xl">{currentProfileData?.username}</p>
+			<p class="text-2xl">{currentProfileData?.age}</p>
 		</div>
 	</div>
 </PageWrapper>
