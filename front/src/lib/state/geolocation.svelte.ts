38594a4738a4e0b5fa2d0177e6b18ae1a903ed6c
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-11-25 23:05:53 +0100

    Migrate geolocation state to runes

---
+++ front/src/lib/state/geolocation.svelte.ts
@@ -0,0 +1,22 @@
+class UserLocation {
+	location = $state<null | [number, number]>(null);
+
+	get value(): null | [number, number] {
+		return this.location;
+	}
+
+	set value(newValue: null | [number, number]) {
+		this.location = newValue;
+	}
+
+	getUserLocation = async () => {
+		if (navigator.geolocation) {
+			console.log('Locating user…');
+			navigator.geolocation.getCurrentPosition((position) => {
+				this.location = [position.coords.latitude, position.coords.longitude];
+				console.log('User located', this.location);
+			});
+		}
+	};
+}
+export const userLocation = new UserLocation();
