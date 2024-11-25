34da37804b7234891f22a3f153b3bbea59f0f764
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-11-25 19:49:34 +0100

    Show map and fame rating on Profile page

--- front/src/lib/interfaces/user-profile-data.interface.ts
+++ front/src/lib/interfaces/user-profile-data.interface.ts
@@ -1,6 +1,11 @@
 export default interface UserProfileData {
 	age: number;
 	biography: string;
+	fameRating: number;
+	location: {
+		type: 'Point';
+		coordinates: [number, number];
+	};
 	gender: 'male' | 'female' | 'other';
 	photos: {
 		is_profile: boolean;
