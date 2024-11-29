a42939cdc2c2117fd8cbd09d4507330529bda577
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-11-29 20:39:53 +0100

    Move chat page to layout

---
+++ front/src/routes/chat/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+	import PageWrapper from '$lib/components/PageWrapper.svelte';
+    const {children} = $props();
+
+    const {conversations} = $page.data;
+</script>
+
+<PageWrapper>
+	<h1>Chat</h1>
+    <h2>List of chats</h2>
+    <ul>
+        {#each conversations as conversation}
+        {$inspect(conversation)}
+            <li>{conversation}</li>
+        {/each}
+    </ul>
+    {@render children()}
+</PageWrapper>
