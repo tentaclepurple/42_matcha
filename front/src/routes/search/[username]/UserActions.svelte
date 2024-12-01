6c0bfa8cd75c1efafe439cd15c69fd78578ec231
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-12-01 20:39:53 +0100

    Improve Like button behaviour

--- front/src/routes/search/[username]/UserActions.svelte
+++ front/src/routes/search/[username]/UserActions.svelte
@@ -3,34 +3,43 @@
 	import Button from '$lib/components/Button.svelte';
 	import { SERVER_BASE_URL } from '$lib/constants/api';
 	import deserialize from '$lib/utils/deserialize';
+	import spinnerAnimationData from '$lib/lotties/spinner.json';
+	import Lottie from '$lib/components/Lottie.svelte';
 
 	const { selectedUser } = $props();
-	console.log('miao', selectedUser);
 
 	let isLikedByMe = $state(selectedUser.likeInfo?.likedByMe);
 	let isMatch = $state(selectedUser.likeInfo?.isMatch);
 
+	let isLoading = $state(false);
+
 	const handleMatch = async () => {
+		isLoading = true;
 		const token = localStorage.getItem('access_token');
 
-		const res = await fetch(`${SERVER_BASE_URL}/api/interactions/like/${selectedUser.username}`, {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${token}`,
-				'Content-Type': 'application/json'
+		try {
+			const res = await fetch(`${SERVER_BASE_URL}/api/interactions/like/${selectedUser.username}`, {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${token}`,
+					'Content-Type': 'application/json'
+				}
+			});
+
+			if (!res.ok) {
+				throw new Error('Failed to like user');
 			}
-		});
 
-		if (!res.ok) {
-			console.error(res);
-			return;
+			const data = await res.json();
+
+			const { likeAdded } = deserialize(data);
+
+			isLikedByMe = Boolean(likeAdded);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			isLoading = false;
 		}
-
-		const data = await res.json();
-
-		const { likeAdded } = deserialize(data);
-
-		isLikedByMe = Boolean(likeAdded);
 	};
 </script>
 
@@ -49,19 +58,28 @@
 	{/if}
 
 	<div class="flex flex-col items-center justify-center">
-		<button
-			class={`flex aspect-square items-center justify-center rounded-xl border border-2 border-black ${isMatch ? 'bg-rose-400' : isLikedByMe ? 'bg-rose-200' : 'bg-transparent'} p-2`}
-			aria-label="Match"
-			onclick={handleMatch}
-		>
-			{#if isMatch}
-				<img src="/icons/like/heart-unlock.svg" alt="" class="h-7" />
-			{:else if isLikedByMe}
-				<img src="/icons/like/heart-lock.svg" alt="" class="h-7" />
-			{:else}
-				<img src="/icons/like/heart.svg" alt="" class="h-7" />
-			{/if}
-		</button>
+		{#if isLoading}
+			<button
+				type="button"
+				class="flex aspect-square w-14 cursor-not-allowed items-center justify-center rounded-xl border border-2 border-black p-2 bg-gray-300"
+			>
+				<Lottie animationData={spinnerAnimationData} autoplay={true} loop={true} />
+			</button>
+		{:else}
+			<button
+				class={`flex aspect-square w-14 items-center justify-center rounded-xl border border-2 border-black ${isMatch ? 'bg-rose-400' : isLikedByMe ? 'bg-rose-200' : 'bg-transparent'} p-2`}
+				aria-label="Match"
+				onclick={handleMatch}
+			>
+				{#if isMatch}
+					<img src="/icons/like/heart-unlock.svg" alt="" class="h-7" />
+				{:else if isLikedByMe}
+					<img src="/icons/like/heart-lock.svg" alt="" class="h-7" />
+				{:else}
+					<img src="/icons/like/heart.svg" alt="" class="h-7" />
+				{/if}
+			</button>
+		{/if}
 		<p class="text-sm">
 			{#if isMatch}
 				Matched
