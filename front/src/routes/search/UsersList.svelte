942ab134af8f97ee537d13cc069cf8b3f085c35c
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-12-09 20:34:10 +0100

    Add pagination to UsersList

--- front/src/routes/search/UsersList.svelte
+++ front/src/routes/search/UsersList.svelte
@@ -3,7 +3,16 @@
 	import GenderSymbol from '$lib/components/GenderSymbol.svelte';
 	import getServerAsset from '$lib/utils/get-server-asset';
 
+	const NUM_RESULTS = 15;
+
 	const { results } = $props();
+
+	let currentPage = $state<number>(0);
+	const totalPages = Math.ceil(results.length / NUM_RESULTS);
+
+	const currentResults = $derived(
+		results.slice(currentPage * NUM_RESULTS, currentPage * NUM_RESULTS + NUM_RESULTS)
+	);
 
 	const handleOpenUser = async (event) => {
 		const button = event.currentTarget as HTMLButtonElement;
@@ -13,27 +22,40 @@
 	};
 </script>
 
-<ul class="grid w-full grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3 p-8">
-	{#each results as user (user.user_id)}
-		<li class="flex items-center justify-center rounded-md bg-teal-50 p-3 shadow-md">
+<div class="items-between flex flex-col h-full w-full flex-1">
+	<ul class="grid w-full grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3 p-8">
+		{#each currentResults as user (user.user_id)}
+			<li class="flex items-center justify-center rounded-md bg-teal-50 p-3 shadow-md">
+				<button
+					type="button"
+					data-username={user.username}
+					onclick={handleOpenUser}
+					class="flex w-full flex-col items-center gap-1"
+				>
+					<img
+						src={getServerAsset(user.profile_photo)}
+						alt=""
+						class="mb-2 aspect-square w-full rounded-md object-cover"
+					/>
+					<span class="text-sm font-bold">{user.username}</span>
+					<span class="flex text-xs">
+						{user.age}, {user.gender}
+						<GenderSymbol gender={user.gender} />
+					</span>
+					<span class="text-xs">{user.distance} km away</span>
+				</button>
+			</li>
+		{/each}
+	</ul>
+	<nav class="mt-auto flex items-center justify-center gap-4 pb-6">
+		{#each Array.from({ length: totalPages }, (_, i) => i) as page, index}
 			<button
 				type="button"
-				data-username={user.username}
-				onclick={handleOpenUser}
-				class="flex w-full flex-col items-center gap-1"
+				class={`${index === currentPage ? 'underline' : ''}`}
+				onclick={() => (currentPage = index)}
 			>
-				<img
-					src={getServerAsset(user.profile_photo)}
-					alt=""
-					class="mb-2 aspect-square object-cover w-full rounded-md"
-				/>
-				<span class="text-sm font-bold">{user.username}</span>
-				<span class="flex text-xs">
-					{user.age}, {user.gender}
-					<GenderSymbol gender={user.gender} />
-				</span>
-				<span class="text-xs">{user.distance} km away</span>
+				{index + 1}
 			</button>
-		</li>
-	{/each}
-</ul>
+		{/each}
+	</nav>
+</div>
