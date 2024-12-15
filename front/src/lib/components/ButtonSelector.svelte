c028e8c8e3befeb1ffffbd4e12e4c722eb6dd4a4
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-12-15 22:10:27 +0100

    Add Gender and Sexual preference basic filters

---
+++ front/src/lib/components/ButtonSelector.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<div class="border-1 w-fit rounded-3xl border border-teal-500 bg-white px-3 py-2 text-xs">
+	{@render children()}
+</div>
--- front/src/routes/search/FiltersAndSorting.svelte
+++ front/src/routes/search/FiltersAndSorting.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
+	import ButtonSelector from '$lib/components/ButtonSelector.svelte';
+	import GenderSymbol from '$lib/components/GenderSymbol.svelte';
+	import PreferenceSymbol from '$lib/components/PreferenceSymbol.svelte';
 	import {
 		DEFAULT_ALL_RESULTS_SORTING_ORDER,
 		DEFAULT_ALL_RESULTS_SORTING_PROP,
 		SORTING_LS_KEY,
 		SORTING_ORDER
 	} from '$lib/constants/sorting';
+	import { GENDER_OPTIONS, PREFERENCES_OPTIONS } from '$lib/constants/user-profile-data';
 	import { onMount } from 'svelte';
 
-	const { results, setResults } = $props();
+	const { results, setResults, setUnfilteredResults, unfilteredResults } = $props();
 
 	let currentFilter = $state(
 		`${DEFAULT_ALL_RESULTS_SORTING_PROP};${DEFAULT_ALL_RESULTS_SORTING_ORDER}`
@@ -21,7 +25,7 @@
 		}
 	});
 
-	const handleFilterChange = (event: Event) => {
+	const handleSortingChange = (event: Event) => {
 		const target = event.target as HTMLSelectElement;
 		const [prop, order] = target.value.split(';');
 
@@ -33,29 +37,75 @@
 			}
 		});
 
-		setResults(orderedResults);
+		setUnfilteredResults(orderedResults);
+		setResults(unfilteredResults);
 		localStorage.setItem(SORTING_LS_KEY, target.value);
+	};
+
+	const handleFilterChange = (event: Event, prop: string) => {
+		const target = event.target as HTMLSelectElement;
+		const filter = target.value;
+
+		const filteredResults = [...unfilteredResults].filter((result) => result[prop] === filter);
+
+		setResults(filteredResults);
 	};
 </script>
 
-<div class="px-6">
-	<label class="border-1 rounded-3xl border border-teal-500 bg-white px-4 py-2 text-xs">
-		Sort by:
-		<select name="filter" onchange={handleFilterChange}>
-			<option value="distance;asc" selected={currentFilter === 'distance;asc'}>
-				distance (nearest)
-			</option>
-			<option value="distance;desc" selected={currentFilter === 'distance;desc'}>
-				distance (farthest)
-			</option>
-			<option value="age;asc" selected={currentFilter === 'age;asc'}>age (youngest)</option>
-			<option value="age;desc" selected={currentFilter === 'age;desc'}>age (oldest)</option>
-			<option value="fame_rating;asc" selected={currentFilter === 'fame_rating;asc'}>
-				popularity (lowest)
-			</option>
-			<option value="fame_rating;desc" selected={currentFilter === 'fame_rating;desc'}>
-				popularity (highest)
-			</option>
-		</select>
-	</label>
+<div class="flex items-center gap-2 px-4">
+	<ButtonSelector>
+		<label>
+			Sort by:
+			<select onchange={handleSortingChange}>
+				<option value="distance;asc" selected={currentFilter === 'distance;asc'}>
+					distance (nearest)
+				</option>
+				<option value="distance;desc" selected={currentFilter === 'distance;desc'}>
+					distance (farthest)
+				</option>
+				<option value="age;asc" selected={currentFilter === 'age;asc'}>age (youngest)</option>
+				<option value="age;desc" selected={currentFilter === 'age;desc'}>age (oldest)</option>
+				<option value="fame_rating;asc" selected={currentFilter === 'fame_rating;asc'}>
+					popularity (lowest)
+				</option>
+				<option value="fame_rating;desc" selected={currentFilter === 'fame_rating;desc'}>
+					popularity (highest)
+				</option>
+			</select>
+		</label>
+	</ButtonSelector>
+
+	<ButtonSelector>
+		<label>
+			Gender:
+			<select onchange={(e) => handleFilterChange(e, 'gender')}>
+				<option value={GENDER_OPTIONS.MALE}>
+					male <GenderSymbol gender={GENDER_OPTIONS.MALE} />
+				</option>
+				<option value={GENDER_OPTIONS.FEMALE}>
+					female <GenderSymbol gender={GENDER_OPTIONS.FEMALE} />
+				</option>
+				<option value={GENDER_OPTIONS.OTHER}>
+					other <GenderSymbol gender={GENDER_OPTIONS.OTHER} />
+				</option>
+			</select>
+		</label>
+	</ButtonSelector>
+
+	<ButtonSelector>
+		<label>
+			Likes:
+			<select onchange={(e) => handleFilterChange(e, 'sexual_preferences')}>
+				<option value={PREFERENCES_OPTIONS.FEMALE}>
+					female <PreferenceSymbol preference={PREFERENCES_OPTIONS.FEMALE} />
+				</option>
+				<option value={PREFERENCES_OPTIONS.MALE}>
+					male <PreferenceSymbol preference={PREFERENCES_OPTIONS.MALE} />
+				</option>
+				<option value={PREFERENCES_OPTIONS.BISEXUAL}>
+					both <PreferenceSymbol preference={PREFERENCES_OPTIONS.BISEXUAL} />
+				</option>
+			</select>
+		</label>
+	</ButtonSelector>
 </div>
