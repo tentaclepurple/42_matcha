2dd6dd3d07660f8817852b7553099580c5df36dc
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-12-15 22:07:16 +0100

    Add empty state to UsersList

--- front/src/routes/search/UsersList.svelte
+++ front/src/routes/search/UsersList.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import GenderSymbol from '$lib/components/GenderSymbol.svelte';
+	import Lottie from '$lib/components/Lottie.svelte';
 	import PreferenceSymbol from '$lib/components/PreferenceSymbol.svelte';
 	import getServerAsset from '$lib/utils/get-server-asset';
+	import emptyAnimationData from '$lib/lotties/empty-users.json';
 
 	const NUM_RESULTS = 15;
 
@@ -23,45 +25,57 @@
 	};
 </script>
 
-<div class="items-between flex h-full w-full flex-1 flex-col">
-	<ul class="grid w-full grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3 p-8">
-		{#each currentResults as user (user.user_id)}
-			<li class="flex items-center justify-center rounded-md bg-teal-50 p-3 shadow-md">
-				<button
-					type="button"
-					data-username={user.username}
-					onclick={handleOpenUser}
-					class="flex w-full flex-col items-center gap-1 text-xs"
-				>
-					<img
-						src={getServerAsset(user.profile_photo)}
-						alt=""
-						class="mb-2 aspect-square w-full rounded-md object-cover"
-					/>
-					<span class="text-sm font-bold">{user.username}</span>
-					<span class="flex">
-						{user.age}, {user.gender}
-						<GenderSymbol gender={user.gender} />
-					</span>
-					<span>
-						Likes: {user.sexual_preferences}
-						<PreferenceSymbol preference={user.sexual_preferences} />
-					</span>
-					<span>{user.distance} km away</span>
-					<span>{user.fame_rating}% popularity</span>
-				</button>
-			</li>
-		{/each}
-	</ul>
-	<nav class="mt-auto flex items-center justify-center gap-4 pb-6">
-		{#each Array.from({ length: totalPages }, (_, i) => i) as page, index}
-			<button
-				type="button"
-				class={`${index === currentPage ? 'underline' : ''}`}
-				onclick={() => (currentPage = index)}
-			>
-				{index + 1}
-			</button>
-		{/each}
-	</nav>
+<div class="mt-12 flex items-center justify-center">
+	{#if results.length === 0}
+		<div class="flex flex-col items-center">
+			<h2 class="mb-2">It's lonely in here</h2>
+			<p class="mb-6">There are no users with these filters. Try again with different filters.</p>
+			<div class="w-96">
+				<Lottie animationData={emptyAnimationData} loop={true} autoplay={true} />
+			</div>
+		</div>
+	{:else}
+		<div class="items-between flex h-full w-full flex-1 flex-col">
+			<ul class="grid w-full grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3 p-8">
+				{#each currentResults as user (user.user_id)}
+					<li class="flex items-center justify-center rounded-md bg-teal-50 p-3 shadow-md">
+						<button
+							type="button"
+							data-username={user.username}
+							onclick={handleOpenUser}
+							class="flex w-full flex-col items-center gap-1 text-xs"
+						>
+							<img
+								src={getServerAsset(user.profile_photo)}
+								alt=""
+								class="mb-2 aspect-square w-full rounded-md object-cover"
+							/>
+							<span class="text-sm font-bold">{user.username}</span>
+							<span class="flex">
+								{user.age}, {user.gender}
+								<GenderSymbol gender={user.gender} />
+							</span>
+							<span>
+								Likes: {user.sexual_preferences}
+								<PreferenceSymbol preference={user.sexual_preferences} />
+							</span>
+							<span>{user.distance} km away</span>
+							<span>{user.fame_rating}% popularity</span>
+						</button>
+					</li>
+				{/each}
+			</ul>
+			<nav class="mt-auto flex items-center justify-center gap-4 pb-6">
+				{#each Array.from({ length: totalPages }, (_, i) => i) as page, index}
+					<button
+						type="button"
+						class={`${index === currentPage ? 'underline' : ''}`}
+						onclick={() => (currentPage = index)}
+					>
+						{index + 1}
+					</button>
+				{/each}
+			</nav>
+		</div>
+	{/if}
 </div>
