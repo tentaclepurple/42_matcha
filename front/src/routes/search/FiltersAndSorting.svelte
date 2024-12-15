edb9ef13ce9d1d3df432af8dde3a002f98e7e0dc
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-12-15 19:04:52 +0100

    Add filters for age and popularity

--- front/src/routes/search/FiltersAndSorting.svelte
+++ front/src/routes/search/FiltersAndSorting.svelte
@@ -29,13 +29,21 @@
 
 <div class="px-6">
 	<label class="border-1 rounded-3xl border border-teal-500 bg-white px-4 py-2 text-xs">
-		Filter by:
+		Sort by:
 		<select name="filter" onchange={handleFilterChange}>
 			<option value="distance;asc" selected={currentFilter === 'distance;asc'}>
 				distance (nearest first)
 			</option>
 			<option value="distance;desc" selected={currentFilter === 'distance;desc'}>
 				distance (farthest first)
+			</option>
+			<option value="age;asc" selected={currentFilter === 'age;asc'}>age (youngest first)</option>
+			<option value="age;desc" selected={currentFilter === 'age;desc'}>age (oldest first)</option>
+			<option value="fame_rating;asc" selected={currentFilter === 'fame_rating;asc'}>
+				Popularity (lowest first)
+			</option>
+			<option value="fame_rating;desc" selected={currentFilter === 'fame_rating;desc'}>
+				Popularity (highest first)
 			</option>
 		</select>
 	</label>
