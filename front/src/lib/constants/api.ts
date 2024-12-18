190d5b5c6f9fefb68b855fd1e10352b63f82e0fa
tentaclepurple <wolfframio@gmail.com>
2024-12-18 01:31:17 +0100

    build npm

--- front/src/lib/constants/api.ts
+++ front/src/lib/constants/api.ts
@@ -1,2 +1,6 @@
-export const SERVER_BASE_URL: string = 'http://localhost:5000';
+export const SERVER_BASE_URL: string = import.meta.env.PROD 
+  ? 'https://imontero.me/matcha'  // En producción
+  : 'http://localhost:5000';      // En desarrollo
+
+//export const SERVER_BASE_URL: string = 'http://localhost:5000';
 //export const SERVER_BASE_URL: string = 'http://51.195.220.248:5000';
