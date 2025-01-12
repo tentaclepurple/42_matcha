0110a572798f518e2ef987773c01ecf01cf04252
tentaclepurple <wolfframio@gmail.com>
2025-01-12 20:06:11 +0100

    atlas

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: up all down exec env clean
 
-all: env up
+all: up
 
 up:
 	docker compose up -d
@@ -13,16 +13,6 @@ exec:
 
 run:
 	cd matcha_backend && python3 run.py
-
-env:
-	@if [ -f .env ]; then \
-		echo ".env file already exists. Skipping .env creation."; \
-	else \
-		read -p "Enter your key: " key; \
-		echo "Downloading .env file..."; \
-		curl -L "https://www.$$key.com/scl/fi/xq4kwbk1z5rpwy1etgr8l/env?rlkey=fl913obdcaddlo9m06ced0ng9&dl=1" -o .env; \
-		echo ".env file has been downloaded."; \
-	fi
 
 logs:
 	docker logs backend
--- matcha_backend/app/__init__.py
+++ matcha_backend/app/__init__.py
@@ -31,11 +31,18 @@ def create_app():
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=40)
 
-    app.config["MONGO_URI"] = (
+    """ app.config["MONGO_URI"] = (
         f"mongodb://{os.getenv('MONGO_ROOT_USERNAME')}:"
         f"{os.getenv('MONGO_ROOT_PASSWORD')}@mongodb:27017/"
         f"{os.getenv('MONGO_DATABASE')}?authSource=admin"
+    ) """
+
+    app.config["MONGO_URI"] = (
+        f"mongodb+srv://{os.getenv('MONGO_ROOT_USERNAME')}:"
+        f"{os.getenv('MONGO_ROOT_PASSWORD')}@woprcluster.s4jjn.mongodb.net/"
+        f"{os.getenv('MONGO_DATABASE')}?retryWrites=true&w=majority&appName=woprcluster"
     )
+
 
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
